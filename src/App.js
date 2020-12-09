192e29d5517e0090c6f4b369db01fff479461424
outhanchazima <outhan254@gmail.com>
2020-12-09 07:25:08 +0300

    working navigation bar

--- src/App.js
+++ src/App.js
@@ -1,9 +1,17 @@
-import './App.css';
+import Navbar from './components/layout/Navbar';
+import Footer from './components/layout/Footer'
 
 function App() {
   return (
-    <div>
+    <div className="container-fluid csnav" >
+      <div className="top">
+        <Navbar/>
+      </div>
+      
       <h1>Hello World</h1>
+      <div>
+        <Footer/>
+      </div>
     </div>
   );
 }
